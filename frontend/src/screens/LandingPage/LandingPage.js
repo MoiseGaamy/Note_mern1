f36e96036592c4ecb8305c0fc8bdc2c3693780d9
Gamy <flokygamy@gmail.com>
2022-01-10 07:55:12 +0000

    added landingPage css

---
+++ frontend/src/screens/LandingPage/LandingPage.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Button, Col, Container, Row } from 'react-bootstrap'
+import './LandingPage.css';
+const LandingPage = () => {
+    return (
+        <div className="main">
+            <Container>
+                <Row>
+                    <Col md="6"></Col>
+                    <Col md="6">
+                        <div className='intro-text'>
+                        <div>
+                            <h className="title">Welcome to Your Note Book</h>
+                            <p className='subtitle'>One Safe place for all your Notes</p>
+                        </div>
+                        <div className='buttonContainer'>
+                            <a href='/login'>
+                                <Button size='lg' className="landingbutton">Login</Button>
+                            </a>
+                            <a href='/register'><Button size="lg" variant="outline-primary">Sign-up</Button></a>
+                        </div>
+                    </div>
+                    </Col>
+                    
+                </Row>
+            </Container>
+        </div>
+    )
+}
+
+export default LandingPage
