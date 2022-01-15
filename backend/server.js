94f5641380a1e6907fd30c18ab1a1f3e1d5e2949
Gamy <flokygamy@gmail.com>
2022-01-15 02:01:30 +0000

    split the routes and controllers

--- backend/server.js
+++ backend/server.js
@@ -2,23 +2,26 @@ const express = require("express");
 const notes = require('./data/notes.js');
 const dotenv = require('dotenv');
 const connectDB = require("./config/db.js")
+const userRoutes = require('./routes/userRoutes.js');
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware.js");
 
 const app = express();
 dotenv.config()
 connectDB();
+app.use(express.json());
 
-app.get('/', (req, res) =>
-{
-    res.send('server is Running..');
-});
-app.get('/api/notes', (req, res) =>
-{
-    res.json(notes)
-})
-app.get('/api/notes/:id', (req, res) =>
-{
-    const note = notes.find(n => n._id === req.params.id)
-    res.send(note);
-})
+// app.get('/', (req, res) =>
+// {
+//     res.send('server is Running..');
+// });
+// app.get('/api/notes', (req, res) =>
+// {
+//     res.json(notes)
+// })
+
+app.use('/api/users', userRoutes)
+app.use(notFound);
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`server started on port ${PORT}`));
