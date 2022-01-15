38becc8283b3cb6b2a02d95a50027cf3f0c96f37
Gamy <flokygamy@gmail.com>
2022-01-15 04:28:19 +0000

    implemented noteRoutes

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const notes = require('./data/notes.js');
 const dotenv = require('dotenv');
 const connectDB = require("./config/db.js")
 const userRoutes = require('./routes/userRoutes.js');
+const noteRoutes = require('./routes/noteRoutes.js');
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware.js");
 
 const app = express();
@@ -20,6 +21,9 @@ app.use(express.json());
 // })
 
 app.use('/api/users', userRoutes)
+app.use('/api/notes', noteRoutes);
+
+
 app.use(notFound);
 app.use(errorHandler);
 
