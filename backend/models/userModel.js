3ad558aa8a9a8cade8bc3d6c96cd5b6f864c49c2
Gamy <flokygamy@gmail.com>
2022-01-15 02:03:59 +0000

    implemented UserModel

---
+++ backend/models/userModel.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs')
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        require: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    isAdmin: {
+        type: Boolean,
+        required: true,
+        default: false,
+    },
+    pic: {
+        type: String,
+        required: true,
+        default: "https://pixabay.com/fr/illustrations/masculin-personnage-homme-avatar-5768177"
+    },
+},
+    {
+        timestamps: true,
+    }
+);
+
+userSchema.pre('save', async function (next)
+{
+    if (!this.isModified('password'))
+    {
+        next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword)
+{
+    return await bcrypt.compare(enteredPassword, this.password);
+}
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
