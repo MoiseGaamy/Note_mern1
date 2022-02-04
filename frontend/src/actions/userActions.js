b9c66004b80e4cac391df6d6318f8466c1be7a75
Gamy <flokygamy@gmail.com>
2022-02-04 03:13:22 +0000

    added User Register constants

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -7,7 +7,10 @@ import
         USER_LOGOUT,
         USER_REGISTER_REQUEST,
         USER_REGISTER_SUCCESS,
-        USER_REGISTER_FAIL
+        USER_REGISTER_FAIL,
+        USER_UPDATE_REQUEST,
+        USER_UPDATE_SUCCESS,
+        USER_UPDATE_FAIL
     } from "../constants/userConstants.js"
 
 export const login = (email, password) => async (dispatch) =>
@@ -78,3 +81,41 @@ export const register = (name, email, password, pic) => async (dispatch) =>
         });
     }
 };
+
+export const updateProfile = (user) => async (dispatch, getState) =>
+{
+    try {
+        dispatch({
+            type: USER_UPDATE_REQUEST
+        });
+
+        const { userLogin: { userInfo }, } = getState();
+
+        const config = {
+            headers: {
+                "content-type": "multipart/form-data",
+                Authorization: `Bearer ${userInfo.token}`,
+            },
+        };
+        const { data } = await axios.post('/api/users/profile', user, config);
+      
+        dispatch({
+            type: USER_UPDATE_SUCCESS,
+            payload: data
+        });  
+        dispatch({
+            type: USER_LOGIN_SUCCESS,
+            payload: data
+        });    
+        localStorage.setItem("userInfo", JSON.stringify(data));
+
+    } catch (error) {
+        const message = error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message
+        dispatch({
+            type: USER_UPDATE_FAIL,
+            payload: message
+        })
+    }
+}
