5ee91367ff8ba05d32ebd2ba8050bfa4a88ffed1
Gamy <flokygamy@gmail.com>
2022-02-04 02:53:39 +0000

    wrapped registerUser with asyncHandler

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -3,7 +3,7 @@ const User = require('../models/userModel.js');
 const generateToken = require("../utils/generateToken.js");
 
 
-const registerUser = async (req, res) =>
+const registerUser = asyncHandler(async (req, res) =>
 {
     const { name, email, password, pic } = req.body;
     const userExists = await User.findOne({ email });
@@ -35,13 +35,13 @@ const registerUser = async (req, res) =>
         res.status(400)
         throw new Error("Error occured!");
     }
-}
+});
 
-const authUser = async (req, res) =>
+const authUser = asyncHandler(async (req, res) =>
 {
     const { email, password } = req.body;
     
-    const user = await  User.findOne({ email });
+    const user = await User.findOne({ email });
 
     if (user && (await user.matchPassword(password)))
     {
@@ -59,7 +59,38 @@ const authUser = async (req, res) =>
         throw new Error("invalid email or password");
     }
    
-}
+});
+
+const updateUserProfile = asyncHandler(async (req, res) =>
+{
+    const user = await User.findById(req.user._id);
+
+    if (user)
+    {
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+        user.pic = req.body.pic || user.pic;
+
+        if (req.body.password)
+        {
+            user.password = req.body.password;
+        }
+
+        const updatedUser = await user.save();
+
+        res.json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            pic: updatedUser.pic,
+            token: generateToken(updatedUser._id),
+        });
+    } else
+    {
+        res.status(404);
+        throw new Error("user not found");
+    }
+});
 
 
-module.exports = { registerUser, authUser };
+module.exports = { registerUser, authUser, updateUserProfile };
