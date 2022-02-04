8c7e47b4688b415edb7a30db682d2c04e0f5de8d
Gamy <flokygamy@gmail.com>
2022-02-04 02:52:06 +0000

    added path module to the server

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,14 @@ const connectDB = require("./config/db.js")
 const userRoutes = require('./routes/userRoutes.js');
 const noteRoutes = require('./routes/noteRoutes.js');
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware.js");
+const path = require("path");
+
 
 const app = express();
 dotenv.config()
 connectDB();
 app.use(express.json());
+
 
 // app.get('/', (req, res) =>
 // {
@@ -22,6 +25,26 @@ app.use(express.json());
 
 app.use('/api/users', userRoutes)
 app.use('/api/notes', noteRoutes);
+
+
+// deployement //
+__dirname = path.resolve();
+// check if the app is in production mode//
+if (process.env.NODE_ENV === 'production')
+{
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+    app.get('*', (req, res) =>
+    {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+} else
+{
+     app.get('/', (req, res) =>
+ {
+     res.send('server is Running..');
+ });
+}
 
 
 app.use(notFound);
